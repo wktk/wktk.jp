f76dae8e692b74bfcb74bd60f7793f86f728e1a6
k.wakitani <k.wakitani@gmail.com>
2019-02-24 17:01:39 +0900

    Reduce the size of h1 on rootPath

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,8 +13,6 @@ class Layout extends React.Component {
       header = (
         <h1
           style={{
-            ...scale(1.5),
-            marginBottom: rhythm(1.5),
             marginTop: 0,
           }}
         >
