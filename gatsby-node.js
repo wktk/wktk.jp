b1bd6bb0b6d849ea9838cf74fdee48cdf9b6bb07
k.wakitani <k.wakitani@gmail.com>
2019-02-26 01:28:58 +0900

    Revert "Don't set slug in gatsby-node.js"
    This reverts commit d5bd2640add7cf26e9877cfcae9df4f7b1d53d13.

--- gatsby-node.js
+++ gatsby-node.js
@@ -67,6 +67,16 @@ exports.createPages = ({ graphql, actions }) => {
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
 }
 
 const replacePath = path => (path === `/` ? path : path.replace(/\/$/, ``))
