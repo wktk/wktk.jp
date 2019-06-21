f249dea2e41e9196c7bf0e412939dcf1b3f156d8
k.wakitani <k.wakitani@gmail.com>
2019-06-21 17:49:52 +0900

    Drop the GA plugin which is not used

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,12 +70,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Gatsby Starter Blog`,
