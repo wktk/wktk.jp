564af3f37560288fb6c67635ed8893c978b846ca
k.wakitani <k.wakitani@gmail.com>
2019-07-06 02:15:20 +0900

    Add AdSense tag

--- src/html.js
+++ src/html.js
@@ -12,6 +12,8 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {props.headComponents}
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <script>{`(adsbygoogle = window.adsbygoogle || []).push({ google_ad_client: "ca-pub-3703878768228760", enable_page_level_ads: true });`}</script>
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
