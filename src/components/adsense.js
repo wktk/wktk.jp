6708c334da29f0981e356b0ea71f8afe55aed817
k.wakitani <k.wakitani@gmail.com>
2019-07-23 21:53:19 +0900

    Fix indentation

--- src/components/adsense.js
+++ src/components/adsense.js
@@ -7,13 +7,13 @@ export default class AdSense extends React.Component {
 
   render () {
     return (
-        <ins className='adsbygoogle'
-          style={{ display: 'block' }}
-          data-ad-client='ca-pub-3703878768228760'
-          data-ad-slot='2985375000'
-          data-ad-format='auto'
-          data-full-width-responsive='true'
-        />
+      <ins className='adsbygoogle'
+        style={{ display: 'block' }}
+        data-ad-client='ca-pub-3703878768228760'
+        data-ad-slot='2985375000'
+        data-ad-format='auto'
+        data-full-width-responsive='true'
+      />
     );
   }
 }
