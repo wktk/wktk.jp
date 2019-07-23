80fdfd01689802694fdde11e9c11b98f4af0c5a3
k.wakitani <k.wakitani@gmail.com>
2019-07-23 19:57:02 +0900

    Load AdSense on Client Entry

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,3 +4,12 @@ import "typeface-merriweather"
 
 // syntax highlight
 require("prismjs/themes/prism-solarizedlight.css")
+
+const loadAdSense = () => {
+  (window.adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-3703878768228760",
+    enable_page_level_ads: true
+  });
+};
+
+export const onClientEntry = loadAdSense;
