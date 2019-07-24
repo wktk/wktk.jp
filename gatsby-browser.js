9ed651c023947bdb83fd04369ea145e312a6f4b4
k.wakitani <k.wakitani@gmail.com>
2019-07-25 01:15:14 +0900

    Remove loadAdSense() to disable auto ads

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,12 +4,3 @@ import "typeface-merriweather"
 
 // syntax highlight
 require("prismjs/themes/prism-solarizedlight.css")
-
-const loadAdSense = () => {
-  (window.adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: "ca-pub-3703878768228760",
-    enable_page_level_ads: true
-  });
-};
-
-// export const onClientEntry = loadAdSense;
