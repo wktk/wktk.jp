f22ef95c1e8412eb302159f508b506f31b71d2df
k.wakitani <k.wakitani@gmail.com>
2019-08-11 17:46:16 +0900

    Try adding defer to AdSense script

--- src/html.js
+++ src/html.js
@@ -12,7 +12,7 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {props.headComponents}
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <script defer async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
