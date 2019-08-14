de50406c09535e235f4596a333ee668d9989acdb
k.wakitani <k.wakitani@gmail.com>
2019-08-14 12:32:55 +0900

    box-shadow を調整

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,7 +45,7 @@ module.exports = {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 590,
-              wrapperStyle: 'box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.3);',
+              wrapperStyle: 'box-shadow: 0px 0px 10px 10px rgba(0,0,0,0.1);',
             },
           },
           {
