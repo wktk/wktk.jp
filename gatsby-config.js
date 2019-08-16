0dcf93f94cc959fcf2d5e2c830e945e4be736e13
k.wakitani <k.wakitani@gmail.com>
2019-08-16 15:09:02 +0900

    Fix favicon & manifest metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,13 +73,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `wktk.jp`,
+        short_name: `wktk.jp`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/profile-pic.jpg`,
       },
     },
     `gatsby-plugin-remove-serviceworker`,
