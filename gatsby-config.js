e533592f0a680bbd322b02ad1e09f2d168b32e2e
k.wakitani <k.wakitani@gmail.com>
2019-12-13 10:54:03 +0900

    Add gatsby-plugin-robots-txt and support Netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,12 @@
+const {
+  NODE_ENV,
+  URL: NETLIFY_SITE_URL = 'https://wktk.jp',
+  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+  CONTEXT: NETLIFY_ENV = NODE_ENV
+} = process.env;
+const isNetlifyProduction = NETLIFY_ENV === 'production';
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL;
+
 module.exports = {
   siteMetadata: {
     title: `wktk.jp`,
@@ -96,6 +105,27 @@ module.exports = {
       options: {
         siteUrl: `https://wktk.jp`,
       },
+    },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        resolveEnv: () => NETLIFY_ENV,
+        env: {
+          production: {
+            policy: [{ userAgent: '*' }]
+          },
+          'branch-deploy': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null
+          },
+          'deploy-preview': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null
+          }
+        }
+      }
     },
     `gatsby-plugin-netlify`,
     `gatsby-plugin-netlify-cache`,
