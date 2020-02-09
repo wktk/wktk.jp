9fb3fa6a0587a112dd2a21bd8b67b9638c81c139
k.wakitani <k.wakitani@gmail.com>
2020-02-09 20:54:27 +0900

    Add reference to rss.xml

--- src/html.js
+++ src/html.js
@@ -13,6 +13,7 @@ export default function HTML(props) {
         />
         {props.headComponents}
         <script defer async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <link rel="alternate" type="application/rss+xml" href="/rss.xml" title="RSS2.0" />
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
