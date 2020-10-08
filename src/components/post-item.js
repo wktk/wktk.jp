2aa1c74b8243576ce04a7b6bee680e6060f57a25
k.wakitani <k.wakitani@gmail.com>
2020-10-08 13:59:56 +0900

    Fix indentation

--- src/components/post-item.js
+++ src/components/post-item.js
@@ -10,22 +10,22 @@ function PostItem({node}) {
       <h3>
         <Link to={node.fields.slug}>{title}</Link>
       </h3>
-        <div class="post-meta">
-          {node.fields.localdate}
-          <ul class="tags">
-            {node.frontmatter.tags.map(tag =>
-              <li><Link to={`/tags/${tag}/`}>{ tag }</Link></li>
-            )}
-          </ul>
-        </div>
-        {node.frontmatter.featuredImage && <Link to={node.fields.slug}>
-          <Image
-            className="post-thumb"
-            objectFit="cover"
-            fixed={node.frontmatter.featuredImage.childImageSharp.fixed}
-            alt={`「${title}」のサムネイル`}
-          />
-        </Link>}
+      <div class="post-meta">
+        {node.fields.localdate}
+        <ul class="tags">
+          {node.frontmatter.tags.map(tag =>
+            <li><Link to={`/tags/${tag}/`}>{ tag }</Link></li>
+          )}
+        </ul>
+      </div>
+      {node.frontmatter.featuredImage && <Link to={node.fields.slug}>
+        <Image
+          className="post-thumb"
+          objectFit="cover"
+          fixed={node.frontmatter.featuredImage.childImageSharp.fixed}
+          alt={`「${title}」のサムネイル`}
+        />
+      </Link>}
       <p
         dangerouslySetInnerHTML={{
           __html: node.frontmatter.description || node.excerpt,
